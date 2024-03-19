84f9aab7d710c55fe6ffc0e02867afd800fd2cef
Aakash Verma <dev.aakashv@gmail.com>
2024-03-19 22:21:42 +0530

    `add`: styles as per the challenge design

--- src/components/accordian/Accordian.tsx
+++ src/components/accordian/Accordian.tsx
@@ -11,27 +11,27 @@ const CustomIcon = ({ img }: { img: string }) => {
 const items: CollapseProps["items"] = [
   {
     key: "1",
-    label: "What is Frontend Mentor, and how will it help me?",
+    label: <h2 className="font-bold text-[#2f1533]">What is Frontend Mentor, and how will it help me?</h2>,
     children:
-      "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+      <p className="text-[#8c6991]">Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.</p>,
   },
   {
     key: "2",
-    label: "Is Frontend Mentor free?",
+    label: <h3 className="font-bold text-[#2f1533]">Is Frontend Mentor free?</h3>,
     children:
-      "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
+      <p className="text-[#8c6991]">Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.</p>,
   },
   {
     key: "3",
-    label: "Can I use Frontend Mentor projects in my portfolio?",
+    label: <h4 className="font-bold text-[#2f1533]">Can I use Frontend Mentor projects in my portfolio?</h4>,
     children:
-      "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+      <p className="text-[#8c6991]">Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!</p>,
   },
   {
     key: "4",
-    label: "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+    label: <h5 className="font-bold text-[#2f1533]">How can I get help if I'm stuck on a Frontend Mentor challenge?</h5>,
     children:
-      "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+      <p className="text-[#8c6991]">The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.</p>,
   },
 ];
 
@@ -39,6 +39,7 @@ const Accordian: React.FC = () => (
   <Collapse
     items={items}
     bordered={false}
+    className="collapse-component font-work_sans"
     expandIconPosition="right"
     expandIcon={({ isActive }) =>
       isActive ? (
