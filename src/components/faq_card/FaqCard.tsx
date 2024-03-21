bc41dc9bc01f4dff6317c1367ce557932df0c87f
Aakash Verma <dev.aakashv@gmail.com>
2024-03-22 01:32:03 +0530

    `update`: add the media query to make the card responsive and achieve the pixel perfect design

--- src/components/faq_card/FaqCard.tsx
+++ src/components/faq_card/FaqCard.tsx
@@ -4,7 +4,7 @@ import Accordian from "../accordian/Accordian";
 
 const FaqCard = () => {
   return (
-    <div className="bg-white py-4 px-4 w-11/12 m-auto mt-36 rounded-lg">
+    <div className="bg-white py-4 px-4 w-11/12 m-auto mt-36 rounded-lg min-[375px]:w-[86%] min-[375px]:px-5">
         <Row align="middle" gutter={24} justify="start" >
             <Col>
             <img src={STAR_ICON} alt="" className="size-6" />
